e70891dabedd6958b2d6711ce647fd70dc5fa1e2
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-03-26 16:09:35 +0200

    Small test improvement.

--- tests/view/manual/inline-filler-removal.js
+++ tests/view/manual/inline-filler-removal.js
@@ -18,10 +18,19 @@ ClassicEditor
 		window.editor = editor;
 
 		let fillerRemoval = true;
-		document.querySelector( '#filler-removal-toggle' ).addEventListener( 'click', () => {
+
+		// Prevent stealing the focus.
+		document.querySelector( '#filler-removal-toggle' ).addEventListener( 'mousedown', evt => {
+			evt.preventDefault();
+		} );
+
+		document.querySelector( '#filler-removal-toggle' ).addEventListener( 'click', evt => {
 			editor.editing.view._renderer.isComposing = fillerRemoval;
 			console.log( `Filler removal is now ${ fillerRemoval ? 'blocked' : 'allowed' }.` );
 			fillerRemoval = !fillerRemoval;
+
+			// Prevent stealing the focus.
+			evt.preventDefault();
 		} );
 	} )
 	.catch( err => {
